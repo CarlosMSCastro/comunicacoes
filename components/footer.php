5ef61b03fdda1b21dc565f4580df634a052cb407
Carlos <carloscastro96@live.com.pt>
2026-01-06 03:54:13 +0000

    primeira_componentização_da_home.php

---
+++ components/footer.php
@@ -0,0 +1,268 @@
+<!-- Carrousel Ultimos Eventos e Noticias-->
+  <div class="container-fluid p-0 m-0">
+    <div class="row m-0 p-0">
+        <div class="col-12 p-0 m-0">
+
+            <section id="noticias">
+
+                <h3 class="titulo-secundario">Últimas Notícias e Eventos</h3><br>
+
+                <div id="carouselNoticias" class="carousel slide"  data-bs-ride="carousel" data-bs-interval="3500">
+
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselNoticias" data-bs-slide-to="0" class="active"></button>
+                        <button type="button" data-bs-target="#carouselNoticias" data-bs-slide-to="1"></button>
+                        <button type="button" data-bs-target="#carouselNoticias" data-bs-slide-to="2"></button>
+                        <button type="button" data-bs-target="#carouselNoticias" data-bs-slide-to="3"></button>
+                        <button type="button" data-bs-target="#carouselNoticias" data-bs-slide-to="4"></button>
+                        <button type="button" data-bs-target="#carouselNoticias" data-bs-slide-to="5"></button>
+                    </div>
+
+                    <div class="carousel-inner">
+
+                        <div class="carousel-item active">
+                            <div class="row g-0">
+                                
+                                <div class="col-12">
+                                    <img src="imagens/carousel3/1.jpg" class="noticias-img w-100">
+                                </div>
+
+                                <div class="noticias-conteudo">
+                                    <h3 class="noticias-titulo">Conferência — Desafios tecnológicos para a próxima década.</h3>
+                                    <div class="noticias-data">07/12/2007</div>
+
+                                    <p class="noticias-texto">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget rutrum nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget rutrum nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget rutrum nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.
+                                    </p>
+                                    
+                                </div>
+                                <a href="#" class="noticias-btn">VER MAIS</a>
+                            </div>
+                        </div>
+
+
+                        <div class="carousel-item">
+                            <div class="row g-0">
+                                
+                                <div class="col-12">
+                                    <img src="imagens/carousel3/2.png" class="noticias-img w-100">
+                                </div>
+
+                                <div class="noticias-conteudo">
+                                    <h3 class="noticias-titulo">Conferência — Desafios tecnológicos para a próxima década.</h3>
+                                    <div class="noticias-data">07/12/2007</div>
+
+                                    <p class="noticias-texto">
+                                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget rutrum nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget rutrum nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget rutrum nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.
+                                    </p>                                    
+                                </div>
+                                <a href="#" class="noticias-btn">VER MAIS</a>
+                            </div>
+                        </div>
+
+                        <div class="carousel-item">
+                            <div class="row g-0">
+                                
+                                <div class="col-12">
+                                    <img src="imagens/carousel3/3.png" class="noticias-img w-100">
+                                </div>
+
+                                <div class="noticias-conteudo">
+                                    <h3 class="noticias-titulo">Conferência — Desafios tecnológicos para a próxima década.</h3>
+                                    <div class="noticias-data">07/12/2007</div>
+
+                                    <p class="noticias-texto">
+                                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget rutrum nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget rutrum nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget rutrum nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.
+                                    </p>                                    
+                                </div>
+                                <a href="#" class="noticias-btn">VER MAIS</a>
+                            </div>
+                        </div>
+
+                        <div class="carousel-item">
+                            <div class="row g-0">
+                                
+                                <div class="col-12">
+                                    <img src="imagens/carousel3/4.png" class="noticias-img w-100">
+                                </div>
+
+                                <div class="noticias-conteudo">
+                                    <h3 class="noticias-titulo">Conferência — Desafios tecnológicos para a próxima década.</h3>
+                                    <div class="noticias-data">07/12/2007</div>
+
+                                    <p class="noticias-texto">
+                                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget rutrum nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget rutrum nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget rutrum nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.
+                                    </p>                                    
+                                </div>
+                                <a href="#" class="noticias-btn">VER MAIS</a>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="row g-0">
+                                
+                                <div class="col-12">
+                                    <img src="imagens/carousel3/5.png" class="noticias-img w-100">
+                                </div>
+
+                                <div class="noticias-conteudo">
+                                    <h3 class="noticias-titulo">Conferência — Desafios tecnológicos para a próxima década.</h3>
+                                    <div class="noticias-data">07/12/2007</div>
+
+                                    <p class="noticias-texto">
+                                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget rutrum nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget rutrum nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget rutrum nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.
+                                    </p>                                    
+                                </div>
+                                <a href="#" class="noticias-btn">VER MAIS</a>
+                            </div>
+                        </div>
+
+                        <div class="carousel-item">
+                            <div class="row g-0">
+                                
+                                <div class="col-12">
+                                    <img src="imagens/carousel3/6.png" class="noticias-img w-100">
+                                </div>
+
+                                <div class="noticias-conteudo">
+                                    <h3 class="noticias-titulo">Conferência — Desafios tecnológicos para a próxima década.</h3>
+                                    <div class="noticias-data">07/12/2007</div>
+
+                                    <p class="noticias-texto">
+                                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget rutrum nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget rutrum nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget rutrum nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.
+                                    </p>                                    
+                                </div>
+                                <a href="#" class="noticias-btn">VER MAIS</a>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </section>
+
+            <nav id="navbarfundo" class="navbarfundo navbar navbar-expand-lg">
+              <div class="mx-auto navbarfundo-topo">
+
+                  <div class="collapse navbar-collapse" id="navbarFundoDropdown">
+                      <ul class="navbar-nav navbarfundo-menu">
+
+                          <li class="nav-item navbarfundo-item">
+                              <a class="nav-link navbarfundo-link" href="#">Empresa</a>
+                          </li>
+
+                          <li class="nav-item navbarfundo-item">
+                              <a class="nav-link navbarfundo-link" href="#">Destaques</a>
+                          </li>
+
+                          <li class="nav-item navbarfundo-item">
+                              <a class="nav-link navbarfundo-link" href="#">Notícias e Eventos</a>
+                          </li>
+
+                          <li class="nav-item navbarfundo-item">
+                              <a class="nav-link navbarfundo-link" href="#">Soluções</a>
+                          </li>
+
+                          <li class="nav-item navbarfundo-item">
+                              <a class="nav-link navbarfundo-link" href="#">Inovação e Tecnologia</a>
+                          </li>
+
+                          <li class="nav-item navbarfundo-item">
+                              <a class="nav-link navbarfundo-link" href="#">Os Nossos Parceiros</a>
+                          </li>
+
+                          <li class="nav-item navbarfundo-item">
+                              <a class="nav-link navbarfundo-link" href="#">Contactos</a>
+                          </li>
+
+                      </ul>
+                  </div>
+
+              </div>
+            </nav>
+
+        </div>
+    </div>
+  </div>
+
+  <footer>
+    <div class="container-fluid p-0">
+      <div class="row m-0 p-0">
+        <div class="col-12 m-0 p-0 footer-desktop">
+          <div id="footerfundo" class="container-fluid p-0">
+            <div class="row m-0 p-0 justify-content-center" id="footer-logos">
+              <div class="col-auto"><img src="imagens/footer/altice_empresas.svg" class="footer-logo"></div>
+              <div class="col-auto"><img src="imagens/footer/samsung.svg" class="footer-logo" id="samsung"></div>
+              <div class="col-auto"><img src="imagens/footer/dell.svg" class="footer-logo"></div>
+              <div class="col-auto"><img src="imagens/footer/aruba.svg" class="footer-logo"></div>
+              <div class="col-auto"><img src="imagens/footer/hp.svg" class="footer-logo"></div>
+            </div>
+            <br><br>
+            <hr id="footer-linha">
+
+            <div class="row m-0 p-0 justify-content-between align-items-center" id="footer-info">
+
+              <div class="col-auto d-flex align-items-center gap-3" id="footer-social">
+                <img src="imagens/socials/contactos.svg" class="footer-icon">
+                <div class="separator"></div>
+                <img src="imagens/socials/linkedIn.svg" class="footer-icon">
+                <img src="imagens/socials/instagram.svg" class="footer-icon">
+                <img src="imagens/socials/facebook.svg" class="footer-icon">
+              </div>
+
+              <div class="col-auto d-flex align-items-center gap-4" id="footer-certificados">
+                <img src="imagens/footer/livrodeReclamacoes.svg" class="footer-livro">
+                <img src="imagens/footer/ralc.svg" class="footer-consumo">
+              </div>
+
+            </div>
+            
+              <div class="col-auto" id="footer-copyright">
+                Copyright © 2021 Grupo MediaMaster. Todos os direitos reservados.
+              </div>
+
+          </div>
+
+        </div>
+        <div class="col-12 m-0 p-0 footer-mobile">
+          <div id="footerfundo" class="container-fluid p-0">
+            <div class="col-auto justify-content-center d-flex align-items-center gap-3" id="footer-social">
+              <img src="imagens/socials/contactos.svg" class="footer-icon">
+              <div class="separator"></div>
+              <img src="imagens/socials/linkedIn.svg" class="footer-icon">
+              <img src="imagens/socials/instagram.svg" class="footer-icon">
+              <img src="imagens/socials/facebook.svg" class="footer-icon">
+            </div>
+            <div class="row m-0 p-0 justify-content-center" id="footer-logos">
+              <div class="col-auto"><img src="imagens/footer/altice_empresas.svg" class="footer-logo"></div>
+              <div class="col-auto"><img src="imagens/footer/samsung.svg" class="footer-logo" id="samsung"></div>
+              <div class="col-auto"><img src="imagens/footer/dell.svg" class="footer-logo"></div>
+              <div class="col-auto"><img src="imagens/footer/aruba.svg" class="footer-logo"></div>
+              <div class="col-auto"><img src="imagens/footer/hp.svg" class="footer-logo"></div>
+            </div>
+            <br><br>
+            <hr id="footer-linha">
+            
+
+            <div class="row m-0 p-0 justify-content-between align-items-center" id="footer-info">
+
+              <div class="col-auto d-flex align-items-center gap-4" id="footer-certificados">
+                <img src="imagens/footer/livrodeReclamacoes.svg" class="footer-livro">
+                <img src="imagens/footer/ralc.svg" class="footer-consumo">
+              </div>
+
+            </div>
+            
+              <div class="col-auto justify-content-between align-items-center" id="footer-copyright">
+                Copyright © 2021 Grupo MediaMaster. Todos os direitos reservados.
+              </div>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </footer>
+
+</body>
+</html>
---
+++ components/header.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Comunicações</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="fonts/fonts.css">
+
+</head>
+<body>
+
+  <header>
+
+    <div class="container-fluid p-0">
+      <div class="row m-0">
+        <div class="col-12 p-0">
+          <div class="header-top header-desktop">
+            <div class="header-left"></div>
+            <div class="header-center">
+              <a href="index.html">
+                <img id="logoprincipal" src="imagens/LogotipoComunicacoes.png">
+              </a>
+            </div>
+
+            <div class="header-right">
+              <div class="social-icons">
+
+                <a href="#"><img src="imagens/socials/contactos.svg"></a>
+                <div class="separator"></div>
+                <a href="#"><img src="imagens/socials/linkedIn.svg"></a>
+                <a href="#"><img src="imagens/socials/instagram.svg"></a>
+                <a href="#"><img src="imagens/socials/facebook.svg"></a>
+              </div>
+            </div>           
+          </div>
+          <div class="header-top header-mobile">
+            <div class="header-center">
+              <a href="index.html">
+                <img id="logoprincipal" src="imagens/LogotipoComunicacoes.png">
+              </a>
+              <div class="social-icons">
+                <a href="#"><img src="imagens/socials/contactos.svg"></a>
+                <div class="separator"></div>
+                <a href="#"><img src="imagens/socials/linkedIn.svg"></a>
+                <a href="#"><img src="imagens/socials/instagram.svg"></a>
+                <a href="#"><img src="imagens/socials/facebook.svg"></a>
+              </div>
+            </div>          
+          </div>         
+        </div>
+      </div>
+      <div class="row m-0">
+        <div class="col-12 p-0">
+
+          <nav class="navbar navbar-expand-lg navbartopo nav-mobile">
+            <div class="container-fluid p-0 m-0">
+              
+            <button class="navbar-toggler" type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarMobile">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+
+
+              <div class="collapse navbar-collapse" id="navbarMobile">
+                <ul class="navbar-nav mx-auto">
+
+                  <li class="nav-item dropdown">
+                    <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Empresa
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Áreas de atuação</a></li>
+                      <li><a class="dropdown-item" href="#">Historial</a></li>
+                      <li><a class="dropdown-item" href="#">Inovação e tecnologia</a></li>
+                      <li><a class="dropdown-item" href="#">Responsabilidade Social</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Destaques</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Notícias e Eventos</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Soluções
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Telecomunicações</a></li>
+                      <li><a class="dropdown-item" href="#">Televisões e audiovisuais</a></li>
+                      <li><a class="dropdown-item" href="#">Informática e Redes</a></li>
+                      <li><a class="dropdown-item" href="#">Data center e cibersegurança</a></li>
+                      <li><a class="dropdown-item" href="#">Internet of Things</a></li>
+                      <li><a class="dropdown-item" href="#">Assistentes virtuais</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Inovação e Tecnologia
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Inovação e Tecnologia 1</a></li>
+                      <li><a class="dropdown-item" href="#">Inovação e Tecnologia 2</a></li>
+                      <li><a class="dropdown-item" href="#">Inovação e Tecnologia 3</a></li>
+                      <li><a class="dropdown-item" href="#">Inovação e Tecnologia 4</a></li>
+                      <li><a class="dropdown-item" href="#">Inovação e Tecnologia 5</a></li>
+                      <li><a class="dropdown-item" href="#">Inovação e Tecnologia 6</a></li>
+                      <li><a class="dropdown-item" href="#">Inovação e Tecnologia 7</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Os Nossos Parceiros</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Contactos</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          
+          <div id="carousel1" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3500">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#carousel1" data-bs-slide-to="0" class="active" aria-current="true"></button>
+              <button type="button" data-bs-target="#carousel1" data-bs-slide-to="1" ></button>
+              <button type="button" data-bs-target="#carousel1" data-bs-slide-to="2" ></button>
+              <button type="button" data-bs-target="#carousel1" data-bs-slide-to="3" ></button>
+            </div>
+
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="imagens/carousel1/cabecalho_home.png" class="d-block w-100">
+              </div>
+              <div class="carousel-item">
+                <img src="imagens/carousel1/cabecalho_destaque.png" class="d-block w-100">
+              </div>
+              <div class="carousel-item">
+                <img src="imagens/carousel1/cabecalho_contactos.png" class="d-block w-100">
+              </div>
+              <div class="carousel-item">
+                <img src="imagens/carousel1/cabecalho_noticias_e_eventos.png" class="d-block w-100">
+              </div>
+            </div>
+
+          </div>
+
+          <nav class="navbar navbar-expand-lg navbartopo nav-desktop">
+            <div class="container">
+              
+            <button class="navbar-toggler" type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNavDropdown">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+
+
+              <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav mx-auto">
+
+                  <li class="nav-item dropdown">
+                    <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Empresa
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Áreas de atuação</a></li>
+                      <li><a class="dropdown-item" href="#">Historial</a></li>
+                      <li><a class="dropdown-item" href="#">Inovação e tecnologia</a></li>
+                      <li><a class="dropdown-item" href="#">Responsabilidade Social</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Destaques</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Notícias e Eventos</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Soluções
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Telecomunicações</a></li>
+                      <li><a class="dropdown-item" href="#">Televisões e audiovisuais</a></li>
+                      <li><a class="dropdown-item" href="#">Informática e Redes</a></li>
+                      <li><a class="dropdown-item" href="#">Data center e cibersegurança</a></li>
+                      <li><a class="dropdown-item" href="#">Internet of Things</a></li>
+                      <li><a class="dropdown-item" href="#">Assistentes virtuais</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Inovação e Tecnologia
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Inovação e Tecnologia 1</a></li>
+                      <li><a class="dropdown-item" href="#">Inovação e Tecnologia 2</a></li>
+                      <li><a class="dropdown-item" href="#">Inovação e Tecnologia 3</a></li>
+                      <li><a class="dropdown-item" href="#">Inovação e Tecnologia 4</a></li>
+                      <li><a class="dropdown-item" href="#">Inovação e Tecnologia 5</a></li>
+                      <li><a class="dropdown-item" href="#">Inovação e Tecnologia 6</a></li>
+                      <li><a class="dropdown-item" href="#">Inovação e Tecnologia 7</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Os Nossos Parceiros</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Contactos</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+
+        </div>
+      </div>
+    </div>
+
+  </header>
